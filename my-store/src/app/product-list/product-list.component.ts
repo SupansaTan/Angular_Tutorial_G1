a090b72277e431c7ce0450b0d305884163123b8d
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-03-25 14:41:09 +0700

    notify Button add

--- my-store/src/app/product-list/product-list.component.ts
+++ my-store/src/app/product-list/product-list.component.ts
@@ -10,8 +10,11 @@ import {products} from '../products';
 
 export class ProductListComponent{
     products = products
-
+    
     share(){
         window.alert('The products has been shared!')
     }
+    onNotify() {
+        window.alert('You will be notified when the product goes on sale');
+      }
 }
