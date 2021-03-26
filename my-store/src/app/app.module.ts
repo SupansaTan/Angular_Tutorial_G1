424c1a7319b687b3b38a10773f84e07fbbf588aa
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-03-26 21:19:50 +0700

    Button Buy

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -10,14 +10,16 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
+import { CartComponent } from './cart/cart.component';
+import { CartService } from './cart.service';
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,10 +28,11 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       {path:'',component: ProductListComponent},
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
      
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
