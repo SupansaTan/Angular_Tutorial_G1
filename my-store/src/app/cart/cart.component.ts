9bde844654e5bf621c6e7dfc60d9fbd33e690beb
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-03-26 21:31:16 +0700

    add to cart

--- my-store/src/app/cart/cart.component.ts
+++ my-store/src/app/cart/cart.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { CartService } from '../cart.service';
 
 @Component({
     selector: 'app-cart',
@@ -9,13 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-
-    constructor() {
+    items: any;
+    constructor(private cartService:CartService) {
     }
 
 
     ngOnInit() {
+            this.items = this.cartService.getItems();
     }
+
 
 }
 
